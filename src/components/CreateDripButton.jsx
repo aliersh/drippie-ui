ee456abc5c2dfcdacc469fa4aaa833367016445f
aliersh <ariel.diaz.borquez@icloud.com>
2024-05-08 17:01:49 -0400

    style: apply tailwind css classes to style button

--- src/components/CreateDripButton.jsx
+++ src/components/CreateDripButton.jsx
@@ -1,7 +1,13 @@
+import { PlusIcon } from '@heroicons/react/24/solid'
+
 const CreateDripButton = () => {
     return (
-        <button type="button">
-            New Drip
+        <button
+            type="button"
+            className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+            <PlusIcon className='mr-2 -ml-0.5 h-4 w-4'/>
+            <span>New Drip</span>
         </button>
     );
 };
