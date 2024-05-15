263f7982db62e063fa7385c27a9f843115ddf40c
aliersh <ariel.diaz.borquez@icloud.com>
2024-05-15 17:44:31 -0400

    feat(context): add toogleDripStatus function to update drip status

--- src/components/context/DripContext.jsx
+++ src/components/context/DripContext.jsx
@@ -10,8 +10,24 @@ const DripProvider = ({ children }) => {
         setDrips(prevDrips => [...prevDrips, drip]);
     };
 
-    return (
-        <DripContext.Provider value={{ drips, addDrip }}>
+    const toogleDripStatus = (dripName, newStatus) => {
+            setDrips(prevDrips =>  prevDrips.map(drip => {
+                if (Object.keys(drip)[0] === dripName) {
+                    return {
+                        ...drip,
+                        [dripName]: {
+                            ...drip[dripName],
+                            status: newStatus
+                        }
+                    };
+                } else {
+                    return drip;
+                }
+            }))
+        }
+
+        return (
+        <DripContext.Provider value={{ drips, addDrip, toogleDripStatus }}>
             {children}
         </DripContext.Provider>
     );
