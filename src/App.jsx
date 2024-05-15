16bb0c0d8ca2a9fc9be3da2cd9ac96ace9c6e5d3
aliersh <ariel.diaz.borquez@icloud.com>
2024-05-15 14:48:05 -0400

    feat(app): wrap App component with DripProvider for global state access

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { NextUIProvider } from "@nextui-org/react";
+import DripProvider from "./components/context/DripContext";
 import DripNavbar from "./components/Navbar/DripNavbar";
 import DripList from "./components/List/DripList";
 
 const App = () => {
     return (
-        <NextUIProvider>
-            <div className="mx-auto max-w-2xl w-full">
-                <DripNavbar />
-                <DripList />
-            </div>
-        </NextUIProvider>
+        <DripProvider>
+            <NextUIProvider>
+                <div className="mx-auto max-w-2xl w-full">
+                    <DripNavbar />
+                    <DripList />
+                </div>
+            </NextUIProvider>
+        </DripProvider>
     );
 };
 
