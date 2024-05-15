bf76acc816f9668bb7b40ac1e0ebb38481cb4cb4
aliersh <ariel.diaz.borquez@icloud.com>
2024-05-15 16:01:05 -0400

    feat(DripList): integrate useDrip hook to access global drips state

--- src/components/List/DripList.jsx
+++ src/components/List/DripList.jsx
@@ -1,27 +1,9 @@
+import { useDrip } from "../context/useDrip";
 import { Accordion, AccordionItem } from "@nextui-org/react";
 import DripItem from "./DripItem";
 
 const DripList = () => {
-    const drips = [{
-        "First Drip": {
-            status: 1,
-            last: 0,
-            count: 0,
-            config: {
-                reentrant: false,
-                interval: 60,
-                dripcheck: "0x123456789",
-                checkparams: "param1,param2",
-                actions: [
-                    {
-                        target: "0x987654321",
-                        data: "dummy data",
-                        value: 100,
-                    },
-                ],
-            },
-        },
-    }];
+    const { drips } = useDrip();
 
     return (
         <Accordion>
