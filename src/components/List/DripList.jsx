392913814c8df08d508d0e3593b251d050c85d92
aliersh <ariel.diaz.borquez@icloud.com>
2024-05-16 20:03:27 -0400

    feat(ui): switch DripList component to table layout

--- src/components/List/DripList.jsx
+++ src/components/List/DripList.jsx
@@ -1,53 +1,119 @@
 import { useDrip } from "../context/useDrip";
-import { Accordion, AccordionItem } from "@nextui-org/react";
+import { useState } from "react";
+import {
+    Table,
+    TableHeader,
+    TableBody,
+    TableRow,
+    TableCell,
+    Chip,
+    TableColumn,
+} from "@nextui-org/react";
 import { Switch } from "@nextui-org/react";
 import { Button } from "@nextui-org/react";
-import { TrashIcon } from "@heroicons/react/16/solid";
+import { TrashIcon, EyeIcon } from "@heroicons/react/16/solid";
 import DripItem from "./DripItem";
 
 const DripList = () => {
     const { drips, toggleDrip, archiveDrip } = useDrip();
+    const [detailsVisible, setDetailsVisible] = useState({});
+
+    const toggleDetails = (dripName) => {
+        setDetailsVisible((prev) => ({
+            ...prev,
+            [dripName]: !prev[dripName],
+        }));
+    };
 
     return (
-        <Accordion>
+        <div className="space-y-4">
             {drips.map((drip, index) => {
                 const dripName = Object.keys(drip)[0];
                 const isSelected = drip[dripName].status === 2;
+                const isDetailsVisible = detailsVisible[dripName];
+
                 return (
-                    <AccordionItem
-                        key={index}
-                        textValue={dripName}
-                        title={
-                            <div className="flex justify-between">
-                                <span>{dripName}</span>
-                                <div className="flex">
-                                    <Switch
-                                        isSelected={isSelected}
-                                        onChange={() => {
-                                            const newStatus = isSelected
-                                                ? 1
-                                                : 2;
-                                            toggleDrip(dripName, newStatus);
-                                        }}
-                                    />
-                                    <Button
-                                        onPress={() => archiveDrip(dripName)}
-                                        className="min-w-0 h-8"
-                                        color="danger"
-                                        isDisabled={isSelected}
-                                        startContent={
-                                            <TrashIcon className="h-4 w-4" />
-                                        }
-                                    ></Button>
-                                </div>
+                    <div key={index}>
+                        <Table
+                            aria-label={`Drip Table ${dripName}`}
+                            className="w-full"
+                            hideHeader
+                        >
+                            <TableHeader>
+                                <TableColumn className="w-1/2">
+                                    Drip Name
+                                </TableColumn>
+                                <TableColumn className="w-1/6">
+                                    Activate
+                                </TableColumn>
+                                <TableColumn className="w-1/6">
+                                    Status
+                                </TableColumn>
+                                <TableColumn className="w-1/6">
+                                    Actions
+                                </TableColumn>
+                            </TableHeader>
+                            <TableBody>
+                                <TableRow key={dripName} textValue={dripName}>
+                                    <TableCell className="w-1/2">
+                                        {dripName}
+                                    </TableCell>
+                                    <TableCell className="w-1/6">
+                                        <Switch
+                                            isSelected={isSelected}
+                                            onChange={() => {
+                                                const newStatus = isSelected
+                                                    ? 1
+                                                    : 2;
+                                                toggleDrip(dripName, newStatus);
+                                            }}
+                                        />
+                                    </TableCell>
+                                    <TableCell className="w-1/6 items-center">
+                                        {isSelected ? (
+                                            <Chip color="success">Active</Chip>
+                                        ) : (
+                                            <Chip color="warning">
+                                                Inactive
+                                            </Chip>
+                                        )}
+                                    </TableCell>
+                                    <TableCell className="flex space-x-2">
+                                        <Button
+                                            onPress={() =>
+                                                archiveDrip(dripName)
+                                            }
+                                            className="min-w-0 h-8"
+                                            color="danger"
+                                            isDisabled={isSelected}
+                                            startContent={
+                                                <TrashIcon className="h-4 w-4" />
+                                            }
+                                        />
+                                        <Button
+                                            onPress={() =>
+                                                toggleDetails(dripName)
+                                            }
+                                            className="min-w-0 h-8"
+                                            color="primary"
+                                            startContent={
+                                                <EyeIcon className="h-4 w-4" />
+                                            }
+                                        />
+                                    </TableCell>
+                                </TableRow>
+                            </TableBody>
+                        </Table>
+                        {isDetailsVisible && (
+                            <div className="mt-4 p-4 z-0 flex flex-col relative justify-between gap-4 bg-content1 overflow-auto rounded-large shadow-small w-full">
+                                <DripItem drip={drip[dripName]} />
                             </div>
-                        }
-                    >
-                        <DripItem drip={drip[dripName]} />
-                    </AccordionItem>
+                        )}
+                    </div>
                 );
             })}
-        </Accordion>
+        </div>
     );
 };
+
 export default DripList;
