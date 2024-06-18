3c4692d54db21d9d2c4ef1803b1432db53aaea72
aliersh <ariel.diaz.borquez@icloud.com>
2024-06-18 18:40:16 -0400

    feat: wrap app with WagmiProvider and QueryClientProvider

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,28 @@ import { NextUIProvider } from "@nextui-org/react";
 import DripProvider from "./components/context/DripContext";
 import DripNavbar from "./components/navbar/DripNavbar";
 import DripList from "./components/list/DripList";
+import { WagmiProvider } from "wagmi";
+import { config } from "./config/config";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const App = () => {
     return (
-        <DripProvider>
-            <NextUIProvider>
-                <div className="mx-auto max-w-2xl w-full">
-                    <DripNavbar />
-                    <DripList />
-                </div>
-            </NextUIProvider>
-        </DripProvider>
+        <WagmiProvider config={config}>
+            <QueryClientProvider client={queryClient}>
+                <DripProvider>
+                    <NextUIProvider>
+                        <div className="mx-auto max-w-2xl w-full">
+                            <DripNavbar />
+                            <DripList />
+                        </div>
+                    </NextUIProvider>
+                </DripProvider>
+            </QueryClientProvider>
+        </WagmiProvider>
+
+        
     );
 };
 
